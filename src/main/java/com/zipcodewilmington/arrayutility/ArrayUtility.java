db8b891abdc86f679da817b1505e1b62e5e85940
igordon215 <igordon215@gmail.com>
2024-08-07 14:22:12 -0400

    startedLab

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -4,4 +4,19 @@ package com.zipcodewilmington.arrayutility;
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility {
+    public Long[] removeValue(Long valueToRemove) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(Long valueToEvaluate) {
+        return null;
+    }
+
+    public Long getMostCommonFromMerge(Long[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer countDuplicatesInMerge(String[] arrayToMerge, String valueToEvaluate) {
+        return null;
+    }
 }
